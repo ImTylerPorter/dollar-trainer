a1f9d13946fb0b3f27ae7bb7c44d4350297136ed
Tyler Porter <hello@cleverladder.com>
2016-11-22 18:32:36 -0600

    Add attributes to user, style and structure account settings forms

---
+++ db/migrate/20161122222918_add_attributes_to_user.rb
@@ -0,0 +1,8 @@
+class AddAttributesToUser < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :bio, :text
+    add_column :users, :trainer, :bool, default: false
+  end
+end
