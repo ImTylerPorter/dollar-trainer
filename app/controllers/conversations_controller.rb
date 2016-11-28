74def27c073cd398ec5eb170b981047b379cc0b7
Tyler Porter <hello@cleverladder.com>
2016-11-28 15:28:22 -0600

    Add view files and CRUD ability to converstaions

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,6 +1,51 @@
 class ConversationsController < ApplicationController
 
+	before_action :find_conversation, only: [:show, :edit, :update, :destroy]
+
 	def index
+	end
+
+	def show
+	end
+
+	def new
+		@conversation = Conversation.new
+	end
+
+	def create
+		@conversation = Conversation.new(conversation_params)
+
+		if @conversation.save
+			redirect_to @conversation
+		else
+			render "new"
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @conversation.update(conversation_params)
+			redirect_to @conversation
+		else
+			render "edit"
+		end
+	end
+
+	def destroy
+		@conversation.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def conversation_params
+		params.require(:conversation).permit(:question)
+	end
+
+	def find_conversation
+		@conversation = Conversation.find(params[:id])
 	end
 
 end
