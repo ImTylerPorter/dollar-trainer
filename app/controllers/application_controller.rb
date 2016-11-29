1864b0e72dd567e755129f02706bdc8511c545e2
Tyler Porter <hello@cleverladder.com>
2016-11-28 22:29:44 -0600

    Add default not answered view, add profile image to view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :find_mike
 
   protected
 
@@ -10,6 +10,12 @@ class ApplicationController < ActionController::Base
   	 devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :update_without_password, :current_password, :bio, :image])
   	 devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :update_without_password, :current_password, :bio, :image])
 
+  end
+
+  private
+
+  def find_mike
+  	@mike = User.find_by_id(5)
   end
 
 end
